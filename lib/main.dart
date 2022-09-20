dee7c0ce3d2518ab803421382d1e6cc02fda268b
Mayank Kantharia <mayankkantharia01@gmail.com>
2022-09-21 00:24:51 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.lightBlue,
-          scaffoldBackgroundColor: Colors.white),
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
       home: const PriceScreen(),
     );
   }
